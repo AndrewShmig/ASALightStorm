2c2361db137a982aef1a96cb2650db6acdfe0a81
AndrewShmig <andrewshmig@gmail.com>
2013-09-16 18:45:54 +0400

    0.26.0
    +AppleDoc documentation added

---
+++ ASALightStorm/AppleDoc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppleDoc
+//
+//  Created by AndrewShmig on 9/16/13.
+//  Copyright (c) 2013 AndrewShmig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
